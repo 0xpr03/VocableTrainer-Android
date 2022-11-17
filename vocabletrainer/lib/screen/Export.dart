c334531b74315aac182649e59f9409a321b14c7d
Aron Heinecke <Ox0p54r36@t-online.de>
2022-11-17 18:37:03 +0100

    testing out inpits for exporting

--- vocabletrainer/lib/screen/Export.dart
+++ vocabletrainer/lib/screen/Export.dart
@@ -16,6 +16,17 @@ class ExportWidget extends StatefulWidget {
   ListViewWidgetWidgetState createState() => ListViewWidgetWidgetState();
 }
 
+enum CSVKind {
+  DEFAULT,
+  EXCEL,
+  RFC4180,
+  TABS,
+  MYSQL,
+  INFORMIX_UNLOAD,
+  INFORMIX_UNLOAD_CSV,
+  CUSTOM
+}
+
 class ListViewWidgetWidgetState extends State<ExportWidget>
     with TickerProviderStateMixin {
   Future<void>? _exportFuture;
@@ -23,6 +34,19 @@ class ListViewWidgetWidgetState extends State<ExportWidget>
   final HashSet<int> _selectedFlag = HashSet();
   late StateStorage cache;
   late TabController tabController;
+  static List<DropdownMenuItem> dropdownItems = [
+    DropdownMenuItem(child: Text("Default"), value: CSVKind.DEFAULT),
+    DropdownMenuItem(child: Text("Excel"), value: CSVKind.EXCEL),
+    DropdownMenuItem(child: Text("RFC 4180"), value: CSVKind.RFC4180),
+    DropdownMenuItem(child: Text("Tabs"), value: CSVKind.TABS),
+    DropdownMenuItem(child: Text("Mysql"), value: CSVKind.MYSQL),
+    DropdownMenuItem(
+        child: Text("Informix UNLOAD"), value: CSVKind.INFORMIX_UNLOAD),
+    DropdownMenuItem(
+        child: Text("Informix UNLOAD CSV"), value: CSVKind.INFORMIX_UNLOAD_CSV),
+    DropdownMenuItem(child: Text("Custom"), value: CSVKind.CUSTOM),
+  ];
+  CSVKind _csvKindSelected = CSVKind.DEFAULT;
 
   bool _exportListMetadata = true;
   bool _exportMultipleLists = true;
@@ -78,8 +102,21 @@ class ListViewWidgetWidgetState extends State<ExportWidget>
         child: Column(children: [
       Text(
           'Export UTF8 encoded CSV file with (multiple) lists.\nSee wiki for more information.'),
-      Checkbox(
+      DropdownButton(
+        items: dropdownItems,
+        hint: Text("Hint"),
+        value: _csvKindSelected,
+        borderRadius: BorderRadius.circular(2.0),
+        icon: const Icon(Icons.arrow_downward),
+        onChanged: (value) {
+          setState(() {
+            _csvKindSelected = value;
+          });
+        },
+      ),
+      CheckboxListTile(
           value: _exportListMetadata,
+          title: Text("Export Metadata (names)"),
           onChanged: (value) {
             setState(() {
               _exportListMetadata = !_exportListMetadata;
